dd58b996f7e076bd3de0259d95fef92577298a58
Nefari0 <chrisdemontigny@yahoo.com>
2022-10-13 13:39:21 -0600

    Keypad component

--- diff-engine/src/Components/Graphs/KeyPad/keypad.styles.jsx
+++ diff-engine/src/Components/Graphs/KeyPad/keypad.styles.jsx
@@ -4,20 +4,7 @@ export const KeyBox = styled.div`
     position: relative;
     height: 200px;
     width: 500px;
-    bottom:-600px;
-    background-color: rgb(240, 240, 240);
-
-    @media (max-width:620px) {
-        transform: scale(0.70);
-        left:-21%;
-        bottom:-320px;
-    }
-
-    @media (max-width:400px) {
-        transform: scale(0.50);
-        left:-21%;
-        bottom:-160px;
-    }
+    bottom:-512px;
 `
 
 export const BaseButton = styled.button`
