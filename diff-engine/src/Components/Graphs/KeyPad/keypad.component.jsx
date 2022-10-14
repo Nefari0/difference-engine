7b48b7e5b354ad70b6ae0ec8b3e6bc4ea72269ab
Nefari0 <chrisdemontigny@yahoo.com>
2022-10-14 04:57:04 -0600

    Gaussian

--- diff-engine/src/Components/Graphs/KeyPad/keypad.component.jsx
+++ diff-engine/src/Components/Graphs/KeyPad/keypad.component.jsx
@@ -1,25 +1,37 @@
 import { KeyBox,BaseButton,LargeButton } from "./keypad.styles";
 import { MathComponent } from "mathjax-react";
-// import { format } from "mathjs";
 
-const KeyPad = ({execute,linearVector,polarVector,formatFunction}) => {
+const KeyPad = (props) => {
+
+    const {
+        execute,
+        linearVector,
+        polarVector,
+        formatFunction,
+        state,
+        changeView
+    } = props
+
+    const { mathFunc } = state 
 
     return (
         <KeyBox>
 
-            <BaseButton onClick={(e) => execute(e,'x^2')}><MathComponent tex={String.raw`${formatFunction('x^2')}`} /></BaseButton>
-            <BaseButton onClick={(e) => execute(e,'x^3')}><MathComponent tex={String.raw`${formatFunction('x^3')}`} /></BaseButton>
+            <BaseButton onClick={(e) => execute(e,'mathFunc','x^2')}><MathComponent tex={String.raw`${formatFunction('x^2')}`} /></BaseButton>
+            <BaseButton onClick={(e) => execute(e,'mathFunc','x^3')}><MathComponent tex={String.raw`${formatFunction('x^3')}`} /></BaseButton>
+
+            <BaseButton onClick={(e) => execute(e,'currentView','gaus')}>Gaussian</BaseButton>
 
             <LargeButton
                 style={{left:'0px'}}
-                onClick={linearVector}
+                onClick={() => linearVector(mathFunc)}
             >
                 Cartesian
             </LargeButton>
 
             <LargeButton
                 style={{right:'0px'}}
-                onClick={polarVector}
+                onClick={() => polarVector(mathFunc)}
             >
                 Polar
             </LargeButton>
