14c025f47d693da56ebbe6defcb61a9efb704e84
Nefari0 <chrisdemontigny@yahoo.com>
2022-10-18 14:51:37 -0600

    Points to sin/cos functions

--- diff-engine/src/Components/Graphs/Plots/Trig/AngleConversion/SinCos/cos.component.jsx
+++ diff-engine/src/Components/Graphs/Plots/Trig/AngleConversion/SinCos/cos.component.jsx
@@ -1,4 +1,4 @@
-import { CosPart,ThetaOrigin } from "../display.styles"
+import { CosPart,ThetaOrigin,CosPointer,PointerOrigin } from "../display.styles"
 
 const Cos = ({radians}) => {
 
@@ -9,9 +9,28 @@ const Cos = ({radians}) => {
         zIndex:'0'
     }
 
+    const condition1 = Math.cos(parseFloat(radians)) < 0
+    const condition2 = Math.sin(parseFloat(radians)) < 0
+
+    const flipPointer = () => {
+        if (condition1 && condition2) {
+            return {
+                transform:`rotate(180deg)`,
+            }
+        } else if (!condition1 && !condition2) {
+            return {
+                transform:`rotate(180deg)`,
+            }
+        }
+    }
+
     return(
         <ThetaOrigin style={Math.cos(parseFloat(radians)) < 0 ? rotation : null}>
-            <CosPart radians={radians}></CosPart>
+            <CosPart radians={radians}>
+                <PointerOrigin style={flipPointer()}>
+                    <CosPointer radians={radians} />
+                </PointerOrigin>
+            </CosPart>
         </ThetaOrigin>
     )
 }
--- diff-engine/src/Components/Graphs/Plots/Trig/AngleConversion/SinCos/sin.component.jsx
+++ diff-engine/src/Components/Graphs/Plots/Trig/AngleConversion/SinCos/sin.component.jsx
@@ -1,4 +1,4 @@
-import { ThetaOrigin,SinPart } from "../display.styles"
+import { ThetaOrigin,SinPart,SinPointer } from "../display.styles"
 
 const Sin = ({radians}) => {
 
@@ -9,9 +9,34 @@ const Sin = ({radians}) => {
         zIndex:'0'
     }
 
+    const condition1 = Math.cos(parseFloat(radians)) < 0
+    const condition2 = Math.sin(parseFloat(radians)) < 0
+
+    const flipPointer = () => {
+        if (condition1 && condition2) {
+            return {
+                top:'-10px',
+                transform:`rotate(180deg)`
+            }
+        } else if (!condition1 && !condition2) {
+            return {
+                top:'-10px',
+                transform:`rotate(180deg)`
+            }
+        } else {
+            return {
+                right:'0px'
+            }
+        }
+    }
+
     return(
         <ThetaOrigin style={Math.sin(parseFloat(radians)) < 0 ? rotation : null}>
-            <SinPart radians={parseFloat(radians)}></SinPart>
+            <SinPart radians={parseFloat(radians)}>
+                <ThetaOrigin style={flipPointer()}>
+                    <SinPointer radians={radians} />
+                </ThetaOrigin>
+            </SinPart>
         </ThetaOrigin>
     )
 }
