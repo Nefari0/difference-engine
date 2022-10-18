0842a68cea304434bda7e7e2933f0254b340f178
Nefari0 <chrisdemontigny@yahoo.com>
2022-10-18 16:59:10 -0600

    Nav bar and header

--- diff-engine/src/App.js
+++ diff-engine/src/App.js
@@ -1,9 +1,16 @@
 import Graph from './Components/Graphs/graph.component';
-import { AppContainer,Adapter } from './App.styles';
+import { AppContainer,Adapter,Header,ImageContainer } from './App.styles';
+import Nav from './Components/Nav/nav.component';
+import pic from './Components/admin_photo1.jpg'
 
 function App() {
   return (
     <AppContainer>
+      <Header>
+        <Nav />
+        <h1>The Difference Engine</h1>
+        {/* <ImageContainer><img src={pic} /></ImageContainer> */}
+      </Header>
       <Adapter>
         <Graph />
       </Adapter>
--- diff-engine/src/App.styles.jsx
+++ diff-engine/src/App.styles.jsx
@@ -1,8 +1,54 @@
 import styled from "styled-components";
 
+export const Header = styled.header`
+    width:100vw;
+    height:80px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.65);
+
+    display: flex;
+	flex-direction: column;
+	flex-wrap: nowrap;
+	justify-content: space-between;
+	align-items: center;
+	align-content: stretch;
+
+    h1 {
+        font-size:32px;
+        height:40px;
+        font-weight:200;
+        color:#555;
+        text-align:left;
+        padding-left:5px;
+        font-family: 'Silkscreen', cursive;
+        margin:0px;
+
+        @media (max-width:550px) {
+            font-size:20px;
+        }
+    }
+`
+
+export const ImageContainer = styled.div`
+    position:absolute;
+    right:50px;
+    top:20px;
+    width:25px;
+    height:25px;
+    border-radius:50%;
+    background-color:blue;
+    overflow:hidden;
+
+    img {
+        position:absolute;
+        left:-12px;
+        top:-5px;
+        width:50px;
+        height:auto;
+    }
+`
+
 export const AppContainer = styled.main`
     position:relative;
-    font-family: sans-serif;
     text-align: center;
     width:100vw;
     min-height:100vh;
