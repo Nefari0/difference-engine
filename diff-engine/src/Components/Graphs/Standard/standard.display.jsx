395cf9c9b9603f5aaad9e2a949e1de8e06c9f826
Nefari0 <chrisdemontigny@yahoo.com>
2022-10-25 12:24:13 -0600

    Can clear history and. Can copy calculation result to clipboard as text

--- diff-engine/src/Components/Graphs/Standard/standard.display.jsx
+++ diff-engine/src/Components/Graphs/Standard/standard.display.jsx
@@ -1,9 +1,18 @@
 import { Standard,History,HistoryItem } from "./standard.styles"
 import { MathComponent } from "mathjax-react"
+import { TinyButton } from "../KeyPad/keypad.styles"
+import { useState } from "react"
+
+const clearHistButton = {
+    position:'absolute',
+    left:'250px',
+    top:'10px',
+    zIndex:'1'
+}
 
 const StandarMathDisplay = ({state,execute}) => {
 
-    const { calculation,history } = state
+    const { calculation,history,mathFunc } = state
 
     const mappedHistory = history.map((el,i) => {
         return <HistoryItem key={i} onClick={(e) => execute(e,'mathFunc',el[1].toString())}>{el[0] + ' = ' + el[1]}</HistoryItem>
@@ -11,9 +20,27 @@ const StandarMathDisplay = ({state,execute}) => {
 
     return(
         <>
+
+            <TinyButton
+                onClick={(e) => execute(e,'history',[])}
+                style={clearHistButton}
+            >
+                Clear history
+            </TinyButton>
+
             <History>{mappedHistory}</History>
             <Standard>
+
                 {calculation && <MathComponent tex={String.raw`${calculation}`} />}
+
+                <TinyButton
+                    // onClick={copy}
+                    onClick={() => {navigator.clipboard.writeText(calculation)}}
+                    style={{right:'6px',top:'7px'}}
+                >
+                    copy
+                </TinyButton>
+
             </Standard>
         </>
     )
