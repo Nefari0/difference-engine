646824ec71e9378528916fa798ca7b7620bf61f2
Nefari0 <chrisdemontigny@yahoo.com>
2022-11-02 13:36:51 -0600

    updates

--- diff-engine/server/index.js
+++ diff-engine/server/index.js
@@ -22,6 +22,7 @@ app.use(
     
 app.use( express.static( __dirname + '/../build'));
 app.get('*', (req,res) => {
+    // app.use(enforce.HTTPS({ trustProtoHeader: true }));
     res.send(path.join(__dirname, '../build/index.html'))
 })
 
