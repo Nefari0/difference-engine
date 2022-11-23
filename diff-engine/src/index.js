9f458c55614432df5b1d8fa3e8550b8d9be90cd6
Nefari0 <chrisdemontigny@yahoo.com>
2022-11-22 22:03:00 -0700

    PWA

--- diff-engine/src/index.js
+++ diff-engine/src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom/client';
 import Offline from './Components/Offline';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HashRouter, BrowserRouter } from 'react-router-dom';
-const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
+// import reportWebVitals from './reportWebVitals';
+import { register as registerServiceWorker } from './serviceWorkerRegistration';
+// import { HashRouter, BrowserRouter } from 'react-router-dom';
+// const Router = process.env.NODE_ENV === 'development' ? HashRouter : BrowserRouter;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Offline>
-      <Router>
+      {/* <Router> */}
         <App />
-      </Router>
+      {/* </Router> */}
     </Offline>
   </React.StrictMode>
 );
-reportWebVitals();
+// reportWebVitals();
+registerServiceWorker()
