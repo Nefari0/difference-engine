9a934f86c843a9d66566c2ef96f34ca04103a04b
Nefari0 <chrisdemontigny@yahoo.com>
2022-12-07 16:40:39 -0700

    Spaces in Python script

--- diff-engine/src/Components/Graphs/GearCalculators/involute.keys.jsx
+++ diff-engine/src/Components/Graphs/GearCalculators/involute.keys.jsx
@@ -40,14 +40,16 @@ const CogKeys = (props) => {
     '\nimport bpy'+
     '\nimport math as math' +
     '\nimport numpy as np' +
+    '\n' +
     `\nz = ${mathFunc} # Number of teeth` +
-    '\nu = np.linspace(0,1,10) # matrix' +
+    '\nu = np.linspace(0,1,20) # matrix' +
     '\nthickness = math.pi/2' +
     '\nref_radius = z/2' +
     '\nbase_radius = ref_radius*.9396950000000001' +
     '\nx = base_radius * ((np.cos(u)) + u * np.sin(u))' +
     '\ny = base_radius * ((np.sin(u)) - u * np.cos(u))' +
     '\ncoords = []' +
+    '\n' +
     '\ndef createMeshFromData(name, origin, verts, edges, faces):' +
     '\n    # Create mesh and object' +
     '\n    me = bpy.data.meshes.new(name+"Mesh")' +
@@ -63,14 +65,19 @@ const CogKeys = (props) => {
     '\n    me.update()' +
     '\nfor i in range(len(x)):' +
     '\n    coords.append([x[i],y[i],0])' +
+    '\n' +
     '\n# --- Thickness parameters and objects --- #' +
     '\nrad_difference = (ref_radius-base_radius)*(.25)' +
-    '\n# Thickess circle' +
+    '\n' +
+    '\n# Thickness circle' +
     '\nbpy.ops.mesh.primitive_circle_add(radius=1.57,enter_editmode=False, location=(ref_radius*math.cos(0), math.sin(rad_difference), 0))' +
+    '\n' +
     '\n# Pitch circle' +
     '\nbpy.ops.mesh.primitive_circle_add(radius=z/2,enter_editmode=False, location=(0, 0, 0))' +
+    '\n' +
     '\n# Outside diameter' +
     '\nbpy.ops.mesh.primitive_circle_add(radius=z/2+1,enter_editmode=False, location=(0, 0, 0))' +
+    '\n' +
     '\n# --- Tooth generator --- #' +
     '\nedges1 = [[len(coords) - 1, 0]]' +
     '\nfor i in range( 0, len(coords)-1):' +
