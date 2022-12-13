358e111768083afeb27f75f6f35006fe21a23de1
Nefari0 <chrisdemontigny@yahoo.com>
2022-12-12 22:31:20 -0700

    Styling and layout

--- diff-engine/src/Components/Graphs/GearCalculators/involute.styles.jsx
+++ diff-engine/src/Components/Graphs/GearCalculators/involute.styles.jsx
@@ -15,3 +15,38 @@ export const ToothWidthContainer = styled.section`
         font-size:40px;
     }
 `
+
+export const CogContainer = styled.div`
+    position:relative;
+    top:-220px;
+    left:-200px;
+    height:400px;
+    width:400px;
+    display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	justify-content: center;
+	align-items: center;
+	align-content: stretch;
+`
+
+export const ReferenceCircle = styled.div`
+    position:relative;
+    width: ${({mathFunc}) => mathFunc*2}px;
+    height: ${({mathFunc}) => mathFunc*2}px;
+    border: solid;
+    border-radius:50%;
+
+    margin:auto;
+
+`
+
+export const CogOrigin = styled.div`
+    height:5px;
+    width:5px;
+    background-color:blue;
+    border-radius:50%;
+    right:0px;
+    top:50%;
+    position:absolute;
+`
