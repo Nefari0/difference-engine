956c29fa0783273f4e334e72b739e227a236f884
Nefari0 <chrisdemontigny@yahoo.com>
2022-12-27 23:24:03 -0700

    Link to Jupyter Complex as a math reference

---
+++ diff-engine/src/Components/Graphs/Informaton/Help/resource.items.jsx
@@ -0,0 +1,12 @@
+import { DocumentContainer } from "./operators.styles";
+
+const Resource = () => {
+    return (
+        <section>
+            <h4>Reference notebook</h4>
+            <a href="https://jupyter.madmodels3d.com/math">The Jupyter Complex</a>
+        </section>
+    )
+}
+
+export default Resource
