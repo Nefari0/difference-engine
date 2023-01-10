a1e3042d685f1c9e029cac969f509214a02cb83a
Nefari0 <chrisdemontigny@yahoo.com>
2023-01-09 23:58:44 -0700

    Using global context for state management

--- diff-engine/src/App.js
+++ diff-engine/src/App.js
@@ -2,13 +2,18 @@ import Graph from './Components/Graphs/graph.component';
 import { AppContainer,Adapter,Header,ImageContainer } from './App.styles';
 import Nav from './Components/Nav/nav.component';
 import pic from './Components/admin_photo1.jpg'
+import { ViewContext } from './Components/Context/view.context';
+import { useContext } from 'react';
 
 function App() {
+
+  const { setCurrentView } = useContext(ViewContext)
+
   return (
     <AppContainer>
       <Header>
         <Nav />
-        <h1>The Difference Engine</h1>
+        <h1 onClick={() => setCurrentView(null)}>The Difference Engine</h1>
         {/* <ImageContainer><img src={pic} /></ImageContainer> */}
       </Header>
       <Adapter>
