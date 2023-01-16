d775478694d23a2114ef323319bed433badcd176
Nefari0 <chrisdemontigny@yahoo.com>
2023-01-16 08:09:58 -0700

    Only returns numerical values

--- diff-engine/src/Components/Graphs/UnitConverter/units.display.jsx
+++ diff-engine/src/Components/Graphs/UnitConverter/units.display.jsx
@@ -10,11 +10,11 @@ const Units = ({state,setState}) => {
     const foot = 0.0003281
 
     const copy = (value) => {
-        navigator.clipboard.writeText(value)
+        if (isNumber(value) != 'input a number') {navigator.clipboard.writeText(value)
         setState({
             ...state,
             alert:`${value} copied to clipboard`
-        })
+        })}
     }
 
     // --- This function converts all incoming input values into inches, which are then converted into all other values 
@@ -30,12 +30,12 @@ const Units = ({state,setState}) => {
         }
     }
 
+    const isNumber = (param) => {return (param != 'NaN' ? param : 'input a number')}
+
     const cm = parseFloat(returnInches()/inch* centimeters).toFixed(10)
     const mm = parseFloat(returnInches()/inch*millimeter).toFixed(10)
     const inches = parseFloat(returnInches()).toFixed(10)
     const ft = parseFloat(returnInches()/inch * foot).toFixed(10)
-
-    console.log(inches)
 
     return(
         <UnitsDisplayContainer>
@@ -49,10 +49,10 @@ const Units = ({state,setState}) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {units !== 'mm' && <tr onClick={() => copy(mm)}><td>{mm}</td><td>mm</td></tr>}
-                    {units !== 'cm' && <tr onClick={() => copy(cm)}><td>{cm}</td><td>cm</td></tr>}
-                    {units !== 'in' && <tr onClick={() => copy(inches)}><td>{inches}</td><td>in</td></tr>}
-                    {units !== 'ft' && <tr onClick={() => copy(ft)}><td>{ft}</td><td>ft</td></tr>}
+                    {units !== 'mm' && <tr onClick={() => copy(mm)}><td>{isNumber(mm)}</td><td>mm</td></tr>}
+                    {units !== 'cm' && <tr onClick={() => copy(cm)}><td>{isNumber(cm)}</td><td>cm</td></tr>}
+                    {units !== 'in' && <tr onClick={() => copy(inches)}><td>{isNumber(inches)}</td><td>in</td></tr>}
+                    {units !== 'ft' && <tr onClick={() => copy(ft)}><td>{isNumber(ft)}</td><td>ft</td></tr>}
                 </tbody>
             </UnitsDisplay>
         </UnitsDisplayContainer>
