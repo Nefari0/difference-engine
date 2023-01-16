6a69e60c81db322bcbfa91984b6d4127c3852934
Nefari0 <chrisdemontigny@yahoo.com>
2023-01-16 07:00:30 -0700

    Cleanup

--- diff-engine/src/Components/Graphs/KeyPad/keypad.component.jsx
+++ diff-engine/src/Components/Graphs/KeyPad/keypad.component.jsx
@@ -14,13 +14,6 @@ const KeyPad = (props) => {
 
     const {setCurrentView} = useContext(ViewContext)
 
-    // console.log(ViewProvider('val'))
-    
-    const changeView = (parameter) => {
-        
-    }
-
-
     const {
         execute,
         linearVector,
@@ -37,7 +30,6 @@ const KeyPad = (props) => {
 
             <BaseButton
                 style={{position:'absolute',right:'10px'}} 
-                // onClick={(e) => execute(e,'currentView','gaus')}
                 onClick={() => setCurrentView('gaus')}
             >
                 <div style={{fontSize:'30px',opacity:'.7'}}><InlineMath math={`\\mu`} /></div>
@@ -46,7 +38,6 @@ const KeyPad = (props) => {
 
             <BaseButton
                 style={{position:'absolute',right:'100px',fontSize:'20px'}}
-                // onClick={(e) => execute(e,'currentView','unit_circle')}
                 onClick={() => setCurrentView('unit_circle')}
             >
                 <InlineMath math={`\\phase{${degrees.toString().substring(0,3)}^\\circ}`} />
@@ -56,7 +47,6 @@ const KeyPad = (props) => {
             {/* FRACTIONS */}
             <BaseButton
                 style={{position:'absolute',right:'100px',top:'85px',fontSize:'30px'}}
-                // onClick={(e) => execute(e,'currentView','fracs')}
                 onClick={(e) => setCurrentView('fracs')}
             >
                 <InlineMath math={`\\frac{${'a'} }{${'b'}}`} />
@@ -65,7 +55,6 @@ const KeyPad = (props) => {
 
             <BaseButton
                 style={{right:'190px'}}
-                // onClick={(e) => execute(e,'currentView','standard')}
                 onClick={(e) => setCurrentView('standard')}
             >
                 {CalculatorIcon()}
@@ -74,10 +63,8 @@ const KeyPad = (props) => {
 
             <BaseButton
                 style={{right:'190px',top:'85px',fontSize:'10px'}}
-                // onClick={(e) => execute(e,'currentView','parabolas')}
                 onClick={() => setCurrentView('parabolas')}
             >
-                {/* <InlineMath math={`\\left(x^{\\smash{2}}\\right)`} /> */}
                 <MathComponent tex={String.raw`${`ax^2+bx+c`}`} />
                 <p>Quadratics</p>
             </BaseButton>
@@ -100,7 +87,6 @@ const KeyPad = (props) => {
 
             <BaseButton
                 style={{right:'10px',top:'85px'}}
-                // onClick={(e) => execute(e,'currentView','gear_calculator')}
                 onClick={() => setCurrentView('gear_calculator')}
             >
                 {CogWheel()}
