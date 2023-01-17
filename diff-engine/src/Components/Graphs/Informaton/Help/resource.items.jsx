cd0f855104b0d53179f2dbcaad9cd0cc08486ab0
Nefari0 <chrisdemontigny@yahoo.com>
2023-01-17 08:58:31 -0700

    Cleaning. Opens new tab when link is clicked

--- diff-engine/src/Components/Graphs/Informaton/Help/resource.items.jsx
+++ diff-engine/src/Components/Graphs/Informaton/Help/resource.items.jsx
@@ -1,10 +1,13 @@
-import { DocumentContainer } from "./operators.styles";
-
 const Resource = () => {
     return (
         <section>
             <h4>Reference notebook</h4>
-            <a href="https://jupyter.madmodels3d.com/math">The Jupyter Complex</a>
+            <a 
+                href="https://jupyter.madmodels3d.com/math"
+                target="_blank"
+            >
+                The Jupyter Complex
+            </a>
         </section>
     )
 }
