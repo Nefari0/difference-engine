20ec95be7835e5639c202f22c2fcce92afdc5ed2
Nefari0 <chrisdemontigny@yahoo.com>
2023-01-20 05:07:52 -0700

    Displaying information about keys is handled through context

--- diff-engine/src/Components/Context/view.context.jsx
+++ diff-engine/src/Components/Context/view.context.jsx
@@ -2,14 +2,22 @@ import { createContext, useState, useEffect } from 'react'
 
 export const ViewContext = createContext({
     currentView:null,
-    setCurrentView:() => null
+    setCurrentView:() => null,
+    displayKeymap:false, // --- Shows details about keys,
+    setDisplayKeymap:() => {}
 })
 
 export const ViewProvider = ({ children }) => {
     
     const [currentView,setCurrentView] = useState(null)
+    const [displayKeymap,setDisplayKeymap] = useState(false)
     
-    const value = {currentView,setCurrentView};
+    const value = {
+        currentView,
+        setCurrentView,
+        displayKeymap,
+        setDisplayKeymap
+    };
     
     return <ViewContext.Provider value={value}>{children}</ViewContext.Provider>
 
