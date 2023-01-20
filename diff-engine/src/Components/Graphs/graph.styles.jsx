6dbceeccf807a02ef280b0dd2cb25814b1e6d0ca
Nefari0 <chrisdemontigny@yahoo.com>
2023-01-20 04:38:08 -0700

    Cleanup

--- diff-engine/src/Components/Graphs/graph.styles.jsx
+++ diff-engine/src/Components/Graphs/graph.styles.jsx
@@ -41,31 +41,6 @@ export const Enclosure = styled.main`
     }
     `
 
-export const ZoomInButton = styled(TinyButton)`
-    right:20px;
-    width:30px;
-    height:30px;
-    // top:-40px;
-    opacity:.4;
-    zIndex:2;
-
-    // @media (max-width:620px) {display:none;}
-`
-export const ZoomOutButton = styled(ZoomInButton)`
-    right:60px;
-`
-
-export const ResetViewButton = styled(ZoomOutButton)`right:105px`
-
-export const ViewSettingsPanel = styled.div`
-    position:absolute;
-    top:-40px;
-    width:150px;
-    right:0px;
-    height:30px;
-    // background-color:blue;
-`
-
 export const Table = styled.div`
     position: absolute;
     opacity:.8;
