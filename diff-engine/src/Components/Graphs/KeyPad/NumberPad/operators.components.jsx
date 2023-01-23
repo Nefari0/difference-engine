3bff75d26c2dcefcc9587602469713eb0bc363da
Nefari0 <chrisdemontigny@yahoo.com>
2023-01-23 07:05:41 -0700

    Arithmetic operator keys are in seperate component

---
+++ diff-engine/src/Components/Graphs/KeyPad/NumberPad/operators.components.jsx
@@ -0,0 +1,62 @@
+import { BaseButton, } from "../keypad.styles";
+import { OperatorPad } from "./nums.styles";
+import { numdata } from "./nums.data";
+import { opsdata } from "./operators.data";
+
+export const Operators = ({styles,state,setState}) => {
+
+    const {mathFunc} = state
+
+    const setItems = (e,val) => {
+
+        const newCharacter = () => {            
+            const mathArr = mathFunc.split('')
+            const previous = mathArr.splice(0,mathArr.length-1,1).join('')
+            return (val.split('').length === 0 ? previous : mathFunc+val)
+        }
+
+        e.preventDefault()
+
+        setState({
+            ...state,
+            mathFunc:newCharacter()
+        })
+    }
+
+    const mappedKeys = numdata.map(el => {
+
+        const display = (el.svg ? (el.svg) : (el.val))
+
+        return (
+            <BaseButton
+                key={el.val}
+                onClick={(e) => setItems(e,el.val)}
+            >
+                <strong>{display}</strong>
+            </BaseButton>
+        )
+    })
+
+    const mappedOperators = opsdata.map(el => {
+
+        const display = (el.svg ? (el.svg) : (el.val))
+
+        return (
+            <BaseButton
+                key={el.val}
+                onClick={(e) => setItems(e,el.val)}
+                style={el.style}
+            >
+                <strong>{display}</strong>
+            </BaseButton>
+        )
+    })
+
+    return (
+
+        <OperatorPad>
+            {/* {mappedKeys} */}
+            {mappedOperators}
+        </OperatorPad>
+    )
+}
---
+++ diff-engine/src/Components/Graphs/KeyPad/NumberPad/operators.data.js
@@ -0,0 +1,25 @@
+export const opsdata = [
+    {
+        // style:{left:'240px'},
+        val:'+',
+        operator:true,
+    },
+
+    {
+        style:{top:'80px'},
+        val:'-',
+        operator:true,
+    },
+
+    {
+        style:{top:'160px'},
+        val:'*',
+        operator:true,
+    },
+
+    {
+        style:{top:'240px'},
+        val:'/',
+        operator:true,
+    },
+]
