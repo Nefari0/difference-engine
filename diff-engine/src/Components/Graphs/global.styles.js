22b6a2654d4bc6bcb4b20e0970736ff8f054891d
Nefari0 <chrisdemontigny@yahoo.com>
2023-01-25 19:16:18 -0700

    Color palette

---
+++ diff-engine/src/Components/Graphs/global.styles.js
@@ -0,0 +1,15 @@
+// color pallete:
+const tier1 = '#FFFFFF'
+const tier2 = '#DADDE2'
+const tier3 = '#B1BDC5'
+const tier4 = '#889FA5'
+const tier5 = '#628281'
+const tier6 = '#43655A'
+
+export const backgroundColors = {
+    paper:'rgb(240, 240, 240)',
+    light:tier1,
+    // light:'#DADDE2',
+    // dark:'#252525'
+    dark:'#43655A',
+}
