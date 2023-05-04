a503fdf7424d06f552ca14e1fcbf6984e997394a
Nefari0 <chrisdemontigny@yahoo.com>
2023-05-03 23:28:44 -0600

    NumberPad can change any text body in state

--- diff-engine/src/Components/Graphs/KeyPad/NumberPad/nums.component.jsx
+++ diff-engine/src/Components/Graphs/KeyPad/NumberPad/nums.component.jsx
@@ -1,39 +1,29 @@
-// import { ViewContext } from "../../../Context/view.context";
-// import { useContext } from "react";
-// import { BaseButton, } from "../keypad.styles";
+import { useEffect, useState } from "react";
 import { NumPad } from "./nums.styles";
 import { numdata } from "./nums.data";
 import Button from "../Button";
 
-const INPUT_TYPE = {
-    mathFunc:'mathfunc',
-    radians:'radians',
-    degrees:'degrees'
-}
+export const NumberPad = ({styles,state,setState,inputField}) => {
 
-export const NumberPad = ({styles,state,setState,inputType}) => {
+    const [input,setInput] = useState('mathFunc')
 
-    const {mathFunc,radians,degrees} = state
-
-    const getInputType = (buttonType = INPUT_TYPE.mathFunc) => ({
-        [INPUT_TYPE.mathFunc]:mathFunc,
-        [INPUT_TYPE.degrees]:degrees,
-        [INPUT_TYPE.radians]:radians
-    },[inputType])
+    useEffect(() => {
+        if (inputField) {setInput(inputField)}
+    },[inputField])
 
     const newCharacter = (inputField,val) => {
         const stringval = inputField.toString()
         const mathArr = stringval.split('')
         const previous = mathArr.splice(0,mathArr.length-1,1).join('')
-        return (val.split('').length === 0 ? previous : mathFunc+val)
+
+        return (val.split('').length === 0 ? previous : state[input]+val)
     }
+
     const setItems = (e,val) => {
-
         e.preventDefault()
-
         setState({
             ...state,
-            mathFunc:newCharacter(mathFunc,val)
+            [input]:newCharacter(input,val)
         })
     }
 
@@ -48,21 +38,12 @@ export const NumberPad = ({styles,state,setState,inputType}) => {
                 styles={el.style}
                 text={display}
             />
-            // <BaseButton
-            //     key={el.val}
-            //     onClick={(e) => setItems(e,el.val)}
-            //     style={el.style}
-            //     darkmode={darkmode}
-            // >
-            //     <strong>{display}</strong>
-            // </BaseButton>
         )
     })
 
     return (
 
         <NumPad
-            // darkmode={darkmode}
             style={styles}
         >
             {mappedKeys}
