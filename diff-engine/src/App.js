239630619d57a110259fa7ba9829379461124e1e
Nefari0 <chrisdemontigny@yahoo.com>
2023-05-29 10:32:33 -0600

    Removing un-used vars

--- diff-engine/src/App.js
+++ diff-engine/src/App.js
@@ -1,7 +1,7 @@
 import Graph from './Components/Graphs/graph.component';
 import { AppContainer,Adapter,Header,ImageContainer } from './App.styles';
 import Nav from './Components/Nav/nav.component';
-import pic from './Components/admin_photo1.jpg'
+// import pic from './Components/admin_photo1.jpg'
 import { ViewContext } from './Components/Context/view.context';
 import { useContext,useEffect } from 'react';
 
@@ -17,7 +17,7 @@ function App({appUpdate}) {
 
   useEffect(() => {
     if (appUpdate) {
-    
+      // setAlert('A new version of this app is available. To download the lat')
     }
   },[])
 
