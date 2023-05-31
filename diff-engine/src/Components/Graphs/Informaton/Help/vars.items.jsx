5439c34012e7f720aff19d202242256bb999bf05
Nefari0 <chrisdemontigny@yahoo.com>
2023-05-31 12:00:50 -0600

    Update variables in docs

--- diff-engine/src/Components/Graphs/Informaton/Help/vars.items.jsx
+++ diff-engine/src/Components/Graphs/Informaton/Help/vars.items.jsx
@@ -1,4 +1,9 @@
 import { OperatorTable } from "./info.styles"
+import { min,max } from "../../graph.component"
+
+const total = Math.abs(min)+max
+const minValue = min/100
+const maxValue = max/100
 
 const Vars = ({darkmode}) => {
     return (
@@ -16,7 +21,7 @@ const Vars = ({darkmode}) => {
                 <tbody>
                     <tr>
                         <td>x,y,u</td>
-                        <td>500 Linearly spaced elements:-2.5,2.5 </td>
+                        <td>{total} Linearly spaced elements:{minValue},{maxValue} </td>
                     </tr>
                 </tbody>
             </OperatorTable>
