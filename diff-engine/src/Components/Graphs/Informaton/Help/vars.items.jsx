ab0ac92bbb2add668090c5f67826f3998f25f466
Nefari0 <chrisdemontigny@yahoo.com>
2023-05-31 21:03:01 -0600

    Fixed code error

--- diff-engine/src/Components/Graphs/Informaton/Help/vars.items.jsx
+++ diff-engine/src/Components/Graphs/Informaton/Help/vars.items.jsx
@@ -1,11 +1,11 @@
 import { OperatorTable } from "./info.styles"
 import { min,max } from "../../graph.component"
 
-const total = Math.abs(min)+max
-const minValue = min/100
-const maxValue = max/100
 
 const Vars = ({darkmode}) => {
+    const total = Math.abs(min)+max
+    const minValue = min/100
+    const maxValue = max/100
     return (
         <section>
             <h4>Default Variables:</h4>
