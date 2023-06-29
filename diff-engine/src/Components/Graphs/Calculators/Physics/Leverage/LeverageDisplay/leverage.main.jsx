55c1053daf3ebb58507f5bebdab5e5d1d87989bf
Nefari0 <chrisdemontigny@yahoo.com>
2023-06-28 21:20:45 -0600

    Cleanup

--- diff-engine/src/Components/Graphs/Calculators/Physics/Leverage/LeverageDisplay/leverage.main.jsx
+++ diff-engine/src/Components/Graphs/Calculators/Physics/Leverage/LeverageDisplay/leverage.main.jsx
@@ -32,35 +32,11 @@ const LeverageDisplay = (props) => {
     const d_eNum = Math.abs(parseFloat(d_e))
     const d_rNum = Math.abs(parseFloat(d_r))
     const totalLength = Math.abs(parseFloat(d_rNum+d_eNum))
-    // const totalPercentage = Math.abs(parseFloat((leverTotalLength)) / 100)
-    // const fulcrumDistance = Math.abs(parseFloat(d_e / totalPercentage))
     const resistance = ((d_r/d_e) * F_e).toFixed(2)
-    // const distanceTraveled = d_eNum/totalLength
-    // const leverBarLength = 400
-
-    // const [rotation,setRotation] = useState(-10)
-
-    // const fulcrumParameters = {
-    //     left:`${fulcrumDistance > 100 ? '0' : fulcrumDistance}%`,
-    //     transition: "all 1000ms",
-    //     position:'absolute',
-    // }
-
-    // const leverBarOrigin = { // This is for dispaying rotational distances
-    //     transformOrigin: `${fulcrumDistance > 100 ? '0' : fulcrumDistance}% 0px`,
-    //     transform:`rotate(${rotation}deg)`,
-    //     transition: "all 1000ms",
-    // }
-
-    // const axisOrigin = { // This is for dispaying rotational distances
-    //     transformOrigin: `${fulcrumDistance > 100 ? '0' : fulcrumDistance}% 0px`,
-    //     transition: "all 1000ms",
-    //     top:'-5px'
-    // }
 
     return (
         <LeverageDisplayContainer darkmode={darkmode}>
-            {/* <h1>Leverage</h1> */}
+            
             <h1>Force/Distance Multipliers</h1>
             
             <InputForceValue>
