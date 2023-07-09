e911d1f1f80d750d8d468a3c78cb4efc57d5d569
Nefari0 <chrisdemontigny@yahoo.com>
2023-07-08 23:23:29 -0600

    Updated pyscript

--- diff-engine/src/Components/Graphs/Calculators/GearCalculators/involute.keys.jsx
+++ diff-engine/src/Components/Graphs/Calculators/GearCalculators/involute.keys.jsx
@@ -1,5 +1,5 @@
 import { backButton,ExecuteButton,CopyIcon } from "../../SVG";
-// import { Py1, pyScript } from './gear.py1'
+import { Py1 } from './gear.py1'
 import { KeyBox } from "../../KeyPad/input.styles";
 import Button from "../../KeyPad/Button";
 import InputField from "../../KeyPad/InputField";
@@ -80,132 +80,6 @@ const CogKeys = (props) => {
     const copyScriptMessage = `A Python script that will generate your ${mathFunc} tooth gear tooth profile has been copied to clipboard. Paste and run this script in Blender's script editor to generate your gear tooth profile`
     const copyPitch = `${pitch} saved to clipbaord`
 
-    // -- THE uMax parameter must equal 1 for this script to work in the python script below
-    var gearScript = '# --- build_a_gear.py --- #'+
-    '\nimport bpy'+
-    '\nimport math as math' +
-    '\nimport numpy as np' +
-    '\n' +
-    `\nz = ${mathFunc} # Number of teeth` +
-    `\nu = np.linspace(0,${uMax},20) # matrix` +
-    '\nthickness = math.pi/2' +
-    '\nref_radius = z/2' +
-    '\nbase_radius = ref_radius*.9396950000000001' +
-    '\nx = base_radius * ((np.cos(u)) + u * np.sin(u))' +
-    '\ny = base_radius * ((np.sin(u)) - u * np.cos(u))' +
-    '\ncoords = []' +
-    '\n' +
-    '\ndef createMeshFromData(name, origin, verts, edges, faces):' +
-    '\n    # Create mesh and object' +
-    '\n    me = bpy.data.meshes.new(name+"Mesh")' +
-    '\n    ob = bpy.data.objects.new(name, me)' +
-    '\n    ob.location = origin' +
-    '\n    ob.show_name = False' +
-    '\n    # Link object to scene and make active' +
-    '\n    bpy.context.collection.objects.link(ob)' +
-    '\n    ob.select_set(True)' +
-    '\n    # Create mesh from given verts, faces.' +
-    '\n    me.from_pydata(verts, edges, faces)' +
-    '\n    # Update mesh with new data' +
-    '\n    me.update()' +
-    '\nfor i in range(len(x)):' +
-    '\n    coords.append([x[i],y[i],0])' +
-    '\n' +
-    '\n# --- Thickness parameters and objects --- #' +
-    '\nrad_difference = (ref_radius-base_radius)*(.25)' +
-    '\n' +
-    '\n# Thickness circle' +
-    '\nbpy.ops.mesh.primitive_circle_add(radius=1.57,enter_editmode=False, location=(ref_radius*math.cos(0), math.sin(rad_difference), 0))' +
-    '\n' +
-    '\n# Pitch circle' +
-    '\nbpy.ops.mesh.primitive_circle_add(radius=z/2,enter_editmode=False, location=(0, 0, 0))' +
-    '\n' +
-    '\n# Outside diameter' +
-    '\nbpy.ops.mesh.primitive_circle_add(radius=z/2+1,enter_editmode=False, location=(0, 0, 0))' +
-    '\n' +
-    '\n# --- Tooth generator --- #' +
-    '\nedges1 = [[len(coords) - 1, 0]]' +
-    '\nfor i in range( 0, len(coords)-1):' +
-    '\n    edges1.append( [i, i+1] )' +
-    '\ncreateMeshFromData( "Profile", [0, 0, 0], coords, edges1, [] )'
-
-    const gearScript2 = '\nimport bpy'+
-    '\nfrom math import *' +
-    '\nimport math' +
-    `\n` + 
-    `\nz = ${mathFunc} # Number of teeth` +
-    `\nref_radius = z/2` +
-    `\nbase_radius = ref_radius*.9396950000000001` +    
-    `\nrad_difference = (ref_radius-base_radius)*(.25)` +
-    `\n` + 
-    `\nname_iterator = str(len(bpy.data.objects))` +
-    `\nprofile_name = "Profile_"+name_iterator` +
-    `\nsecondary_profile = 'Profile_'+name_iterator+'.001' # Existing name` +
-    `\nnew_secondary_profile_name = 'Profile_'+name_iterator+'_'+'2'` +
-    `\n` + 
-    `\ngear_name = 'Gear_'+name_iterator` +
-    `\nref_cir_name = 'REFERENCE CIRCLE_'+name_iterator` +
-    `\nthickness_cir_name = 'THICKNESS CIRCLE_'+name_iterator` +
-    `\n` + 
-    `\n# Create empty as parent element` +
-    `\nbpy.ops.object.empty_add(type='ARROWS', location=(0, 0, 0))` +
-    `\nbpy.data.objects['Empty'].name = gear_name` +
-    `\n` + 
-    '\ndef createMeshFromData(name, origin, verts, edges, faces):' +
-    '\n    # Create mesh and object' +
-    `\n    me = bpy.data.meshes.new(name+'Mesh')` +
-    '\n    ob = bpy.data.objects.new(name, me)' +
-    '\n    ob.location = origin' +
-    '\n    ob.show_name = False' +
-    '\n    # Link object to scene and make active' +
-    '\n    bpy.context.collection.objects.link(ob)' +
-    '\n    ob.select_set(True)' +
-    `\n` + 
-    '\n# Create mesh from given verts, faces.' +
-    '\n    me.from_pydata(verts, edges, faces)' +
-    `\n` + 
-    '\n# Update mesh with new data' +
-    '\n    me.update()' +
-    `\n` + 
-    `\nverts1 = ${blenderCoords}` +
-    `\n` + 
-    '\nedges1 = [[len(verts1) - 1, 0]]' +
-    '\nfor i in range( 0, len(verts1)-1):' +
-    '\n    edges1.append( [i, i+1] )' +
-    `\ndel edges1[0]` +
-    `\n` + 
-    `\n# Thickness circle#` +
-    `\nbpy.ops.mesh.primitive_circle_add(radius=1.57,enter_editmode=False, location=(ref_radius*math.cos(0), math.sin(rad_difference), 0))` +
-    `\nbpy.data.objects['Circle'].name = thickness_cir_name` +
-    `\n` + 
-    `\n# Reference circle` +
-    `\nbpy.ops.mesh.primitive_circle_add(radius=z/2,enter_editmode=False, location=(0, 0, 0))` +
-    `\nbpy.data.objects['Circle'].name = ref_cir_name` +
-    `\nbpy.ops.object.select_all(action='DESELECT')` +
-    `\n` + 
-    `\n# Generating Profile` +
-    `\ncreateMeshFromData( profile_name, [0, 0, 0], verts1, edges1, [] )` +
-    `\nbpy.data.objects[profile_name].select_set(True)` +
-    `\n` + 
-    `\nbpy.context.view_layer.objects.active = bpy.data.objects[profile_name]` +
-    `\nbpy.ops.object.duplicate_move(OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, TRANSFORM_OT_translate={"value":(0, 0, 0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, False), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False})` +
-    `\nbpy.ops.transform.rotate(value=3.14159, orient_axis='X')` +
-    `\nbpy.data.objects[secondary_profile].name = new_secondary_profile_name` +
-    `\n` + 
-    `\n### Empty becomes parent` +
-    `\nbpy.data.objects[profile_name].select_set(True)` +
-    `\n#bpy.data.objects[secondary_profile.split('.')[0]].select_set(True)` +
-    `\nbpy.data.objects[new_secondary_profile_name.split('.')[0]].select_set(True)` +
-    `\nbpy.data.objects[ref_cir_name].select_set(True)` +
-    `\nbpy.data.objects[thickness_cir_name].select_set(True)` +
-    `\n` + 
-    `\nobj = bpy.context.window.scene.objects[gear_name]` +
-    `\nbpy.context.view_layer.objects.active = obj` +
-    `\nbpy.ops.object.parent_set(type='OBJECT', keep_transform=True)` +
-    `\n` +
-    `\nbpy.ops.object.select_all(action='DESELECT')` +
-    `\nbpy.data.objects[new_secondary_profile_name].select_set(True)`
-
     const copyVal = (val,name,message) => {
         navigator.clipboard.writeText(val)
         setAlert(message)
@@ -267,84 +141,49 @@ const CogKeys = (props) => {
                 buttonClass={'help'}
             />
 
-                {/* <h2
-                    style={{
-                        position:'absolute',
-                        left:'0px',
-                        top:'150px'
-                    }}
-                >
-                    Pitch: <InlineMath math={`${pitch}^\\circ`} />
-                </h2> */}
+            {pitch != 'Infinity' && parseFloat(mathFunc) >= 5 && <Button
+                styles={{top:'90px',left:'0px',width:'170px',fontSize:'15px',zIndex:'2'}}
+                onClick={() => copyVal(Py1(state),'alert',copyScriptMessage)}
+                text={`Generate Profile`}
+                buttonClass={'large'}
+                p={'Save profile generator script'}
+            />}
 
-                {/* ORIGINAL */}
-                {/* {pitch != 'Infinity' && parseFloat(mathFunc) >= 5 && <Button
-                    styles={{top:'90px',left:'0px',width:'170px',fontSize:'15px',zIndex:'2'}}
-                    // onClick={() => copyVal(pitch,'noticeContent',copyPitch)}
-                    onClick={() => copyVal(gearScript,'alert',copyScriptMessage)}
-                    text={`Generate Profile`}
-                    buttonClass={'large'}
-                    // buttonType={'image'}
-                    p={'Save profile generator script'}
-                />} */}
-                {/* FROM ABOVE */}
-                {pitch != 'Infinity' && parseFloat(mathFunc) >= 5 && <Button
-                    styles={{top:'90px',left:'0px',width:'170px',fontSize:'15px',zIndex:'2'}}
-                    onClick={() => copyVal(gearScript2,'alert','no message added')}
-                    text={`Generate Profile`}
-                    buttonClass={'large'}
-                    p={'Save profile generator script'}
-                />}
+            {pitch != 'Infinity' && parseFloat(mathFunc) >= 5 && <Button
+                styles={{top:'170px',left:'0px',width:'170px',fontSize:'15px',zIndex:'1'}}
+                onClick={() => copyVal(pitch,'noticeContent',copyPitch)}
+                text={`pitch = ${pitch}^\\circ`}
+                buttonType={'image'}
+                p={'copy pitch'}
+            />}
 
-                {/* - THIS BLOCK TESTS COPYING THE INVOLUTE ARRAY */}
-                {/* {pitch != 'Infinity' && parseFloat(mathFunc) >= 5 && <Button
-                    styles={{top:'90px',left:'0px',width:'170px',fontSize:'15px',zIndex:'2'}}
-                    onClick={() => copyVal(involute,'coordinates',"inv coords")}
-                    // onClick={() => copyVal(gearScript,'alert',copyScriptMessage)}
-                    text={`Generate Profile`}
-                    buttonClass={'large'}
-                    // buttonType={'image'}
-                    p={'Save profile generator script'}
-                />} */}
+            <a
+                style={{
+                    position:'absolute',
+                    left:'0px',
+                    fontSize:'30px',
+                    fontWeight:'200',
+                    bottom:'-120px',
+                }}
+                href='https://jupyter.madmodels3d.com/Involute%20Gear%20Calculator'
+                target="_blank"
+            >
+                How to build your gear
+            </a>
 
-                
-
-                {pitch != 'Infinity' && parseFloat(mathFunc) >= 5 && <Button
-                    styles={{top:'170px',left:'0px',width:'170px',fontSize:'15px',zIndex:'1'}}
-                    onClick={() => copyVal(pitch,'noticeContent',copyPitch)}
-                    text={`pitch = ${pitch}^\\circ`}
-                    // buttonClass={'tiny'}
-                    buttonType={'image'}
-                    p={'copy pitch'}
-                />}
-
-                <a
-                    style={{
-                        position:'absolute',
-                        left:'0px',
-                        fontSize:'30px',
-                        fontWeight:'200',
-                        bottom:'-120px',
-                    }}
-                    href='https://jupyter.madmodels3d.com/Involute%20Gear%20Calculator'
-                    target="_blank"
-                >
-                    How to build your gear
-                </a>
-
-                <a
-                    style={{
-                        position:'absolute',
-                        left:'0px',
-                        fontSize:'30px',
-                        fontWeight:'200',
-                        bottom:'-160px',
-                    }}
-                    href="https://www.blender.org/"
-                    target="_blank"
-                >
-                    Download Blender
-                </a>
+            <a
+                style={{
+                    position:'absolute',
+                    left:'0px',
+                    fontSize:'30px',
+                    fontWeight:'200',
+                    bottom:'-160px',
+                }}
+                href="https://www.blender.org/"
+                target="_blank"
+            >
+                Download Blender
+            </a>
 
         </KeyBox>
     )
