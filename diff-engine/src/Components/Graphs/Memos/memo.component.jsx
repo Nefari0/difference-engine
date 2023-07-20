de828424c2ec2bbb0a9a468f753dd2676c2fae96
Nefari0 <chrisdemontigny@yahoo.com>
2023-07-20 13:43:47 -0600

    Notebook component

--- diff-engine/src/Components/Graphs/Memos/memo.component.jsx
+++ diff-engine/src/Components/Graphs/Memos/memo.component.jsx
@@ -1,22 +1,46 @@
-import { useState,useContext } from "react";
+import { useState,useContext,useEffect } from "react";
 import { ViewContext } from "../../Context/view.context";
 import { MemoContainer,MemoOverlay } from "./memo.styles";
 import Button from "../KeyPad/Button";
 import { CloseX,InArrows,OutArrows } from "../SVG";
+const defaultNotebook = { text:'' }
 
 const MemoPad = () => {
-
+    
+    const NOTEBOOK_NAME = 'DIFF_ENGINE_NOTEBOOK'
     const [localState,setLocalState] = useState({
         large:false,
+        text:''
     })
 
-    const { openMemo,setOpenMemo } = useContext(ViewContext)
+    const {large,text} = localState
+    const { openMemo,setOpenMemo,darkmode } = useContext(ViewContext)
+    const savedNoteBook = localStorage.getItem(NOTEBOOK_NAME)
+    // const textAreaValue = JSON.parse(savedNoteBook).text
 
-    const {large} = localState
+    const textHandler = (e) => {
+        e.preventDefault()
+        const {name,value} = e.target
+        setLocalState({...localState,[name]:value})
+        localStorage.setItem(NOTEBOOK_NAME,localState.text)
+    }
+
+    useEffect(() => {
+        try {
+            if (!savedNoteBook) {
+                localStorage.setItem(NOTEBOOK_NAME,JSON.stringify(defaultNotebook))
+            } else {
+                setLocalState({...localState, text:savedNoteBook})
+            }
+        } catch (error) {
+            console.log('HERE IS THE ERROR',error)
+            return
+        }
+    },[])
 
     return (
         <MemoOverlay>
-            <MemoContainer large={large}>
+            <MemoContainer large={large} darkmode={darkmode}>
                 <Button 
                     style={{
                         transform:'scale(.3)',
@@ -37,7 +61,11 @@ const MemoPad = () => {
                     text={large ? InArrows() : OutArrows()}
                 />
                 {/* {CloseX({width:'20px'})} */}
-                <textarea/>
+                <textarea
+                    value={text}
+                    name='text'
+                    onChange={textHandler}
+                />
             </MemoContainer>
         </MemoOverlay>
     )
--- diff-engine/src/Components/Graphs/Memos/memo.styles.jsx
+++ diff-engine/src/Components/Graphs/Memos/memo.styles.jsx
@@ -1,6 +1,8 @@
-import styled from "styled-components";
-// import { widthParameters } from "../global.styles";
-
+import styled, { css } from "styled-components";
+import { backgroundColors } from "../global.styles";
+import { widthParameters } from "../global.styles";
+// import { basicDark,basicLight } from "../global.styles";
+const { enclosureWidth,enclosurePadding } = widthParameters
 export const MemoOverlay = styled.section`
     width:100%;
     height:100%;
@@ -8,27 +10,38 @@ export const MemoOverlay = styled.section`
     justify-content:center;
     align-items:flex-start;
     position:fixed;
-    // background-color:yellow;
     z-index:1;
     pointer-events: none;
 `
 
 // width:${widthParameters.enclosureWidth}px;
+// ${({darkmode}) => darkmode ? basicDark : basicLight}
+const dark = css`
+    background-color:${backgroundColors.lightDark};
+    border: solid 1px ${backgroundColors.paper};
+`
+const light = css`
+    background-color:${backgroundColors.darkLight};
+    border: solid 1px #555;
+`
 export const MemoContainer = styled.section`
-    width:250px;
-    height:${({large})=>large ? 200 : 50}px;
-    top:${({large}) => large ? 0 : 100}px;
-    background-color:blue;
+    ${({darkmode}) => darkmode ? dark : light}
+    width:${(enclosureWidth)*.5}px;
+    padding:${enclosurePadding*.5}px;
+    height:${({large})=>large ? 240 : 100}px;
+    top:${({large}) => large ? 10 : 50}px;
     z-index:11;
-    // opacity:.5;
     pointer-events: auto;
-    position:absolute;
+    position:fixed;
     transition: all 1000ms;
+    border-radius:5px;
 
     textarea {
         position:relative;
         top:30px;
-        width:100%;
-        height:100%;
+        width:95%;
+        height:${({large}) => large ? 200 : 60}px;
+        background-color:${backgroundColors.paper};
+        transition: all 1000ms;
     }
 `
