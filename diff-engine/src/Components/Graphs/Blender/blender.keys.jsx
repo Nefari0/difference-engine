4761c1b3acfaddc42612e278db49ddf2fe3dccbf
Nefari0 <chrisdemontigny@yahoo.com>
2023-08-09 23:41:53 -0600

    Creating item components

--- diff-engine/src/Components/Graphs/Blender/blender.keys.jsx
+++ diff-engine/src/Components/Graphs/Blender/blender.keys.jsx
@@ -7,6 +7,7 @@ import MotorKeys from "./motor.keys";
 import { motor_gen } from "../../../py-scripts/motor-rigging/motor-rig";
 import { create_mesh } from "../../../py-scripts/mesh-generator/create_mesh.py";
 import { studio_setup } from "../../../py-scripts/backdrop-generator/backdrop.py";
+import BlenderLink from "./BlenderItem/item.component";
 
 const alertMessage = 'code has been copied to the clipboard'
 
@@ -17,7 +18,7 @@ const BlenderScripts = (props) => {
         selectedScript:null
     })
     const { selectedScript } = localState
-    const { darkmode,setAlert } = useContext(ViewContext)
+    const { darkmode,setAlert,displayKeymap,setDisplayKeymap } = useContext(ViewContext)
     const copyVal = (val,name,message) => {
         navigator.clipboard.writeText(val)
         setAlert(message)
@@ -28,22 +29,58 @@ const BlenderScripts = (props) => {
     },[])
     
     return (
-        <BlenderKeys darkmode={darkmode}>
+        <BlenderKeys 
+            darkmode={darkmode}
+            displayKeymap={displayKeymap}
+        >
             <Button
                 styles={{
                     right:'0px',
                     zIndex:'1'
                 }}
                 text={!selectedScript ? backButton() : uturnArrow()}
+                buttonClass={'help'}
                 onClick={(e) => {!selectedScript ? close(e) : setLocalState({...localState,selectedScript:null})}}
             />
+
+            <Button
+                styles={{
+                    right:'0px',
+                    zIndex:'1',
+                    top:'80px'
+                }}
+                text={'?'}
+                onClick={(e) => setDisplayKeymap(!displayKeymap)}
+            />
+            
             {!selectedScript &&
                 <>
-                    <a onClick={() => setLocalState({...localState, selectedScript:'motors'})}>motor-rigging</a>
+                    <BlenderLink
+                        onClick={() => setLocalState({...localState, selectedScript:'motors'})}
+                        title={'motor-rigging'}
+                        description={'about'}
 
-                    <a onClick={() => copyVal(create_mesh(),null,alertMessage)}>create mesh</a>
+                    />
+                    
+                    {/* <p style={{top:'0px'}}>message</p> */}
 
-                    <a onClick={() => copyVal(studio_setup(),null,alertMessage)}>studio setup</a>
+                    {/* <a onClick={() => copyVal(create_mesh(),null,alertMessage)}>create mesh</a> */}
+                    <BlenderLink
+                        // onClick={() => setLocalState({...localState, selectedScript:'motors'})}
+                        onClick={() => copyVal(create_mesh(),null,alertMessage)}
+                        title={'create mesh'}
+                        description={'create mesh'}
+                    />
+                    {/* <p>message</p> */}
+
+                    {/* <a onClick={() => copyVal(studio_setup(),null,alertMessage)}>studio setup</a> */}
+                    <BlenderLink
+                        // onClick={() => setLocalState({...localState, selectedScript:'motors'})}
+                        // onClick={() => copyVal(create_mesh(),null,alertMessage)}
+                        onClick={() => copyVal(studio_setup(),null,alertMessage)}
+                        title={'studio setup'}
+                        description={'Instantly add a backdrop with lighting and a camera. The size is scalable'}
+                    />
                 </>
             }
 
