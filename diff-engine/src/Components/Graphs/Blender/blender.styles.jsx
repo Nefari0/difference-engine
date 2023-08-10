19439df4515a608c52504e1998cbcd2e81cfab9c
Nefari0 <chrisdemontigny@yahoo.com>
2023-08-09 23:42:20 -0600

    Creating item components

--- diff-engine/src/Components/Graphs/Blender/blender.styles.jsx
+++ diff-engine/src/Components/Graphs/Blender/blender.styles.jsx
@@ -1,22 +1,19 @@
 import styled from "styled-components";
 import { KeyBox } from "../KeyPad/input.styles";
+import { messageBox } from "../KeyPad/input.styles";
 
 export const BlenderKeys = styled(KeyBox)`
     display:flex;
     flex-direction:column;
     text-align:left;
 
-    a {
-        position:relative;
-        font-size:20px;
-        left:0px;
-        margin:8px;
-        color:${({darkmode}) => darkmode ? '#fff' : '#555'};
-        cursor:pointer;
-    }
-
     form {
         height:300px;
         width:300px;
     }
-`
+
+    `
+    // p {
+    //     ${messageBox}
+    //     display:${({displayKeymap}) => !displayKeymap && 'none'};
+    // }
