529f9c79dfead13a8b4ff8f5a06d16cb231ee923
Nefari0 <chrisdemontigny@yahoo.com>
2023-08-24 21:49:35 -0600

    Added number pad to fraction converter

--- diff-engine/src/Components/Graphs/Calculators/Converters/Fractions/frac.keys.jsx
+++ diff-engine/src/Components/Graphs/Calculators/Converters/Fractions/frac.keys.jsx
@@ -1,23 +1,16 @@
 import {useEffect,useContext} from "react";
 import { ViewContext } from "../../../../Context/view.context";
 import { KeyBox } from "../../../KeyPad/input.styles";
-import InputField from "../../../KeyPad/InputField";
 import Button from "../../../KeyPad/Button";
 import { uturnArrow } from "../../../SVG";
-
-const errorMessage = 'Only numbers can be entered into this input field. Note: A decimal point must be present as the first element'
+import { NumberPad } from "../../../KeyPad/NumberPad/nums.component";
 
 const FractionKeys = (props) => {
 
     const {
         state,
         setState,
-        inputHandler,
-        execute,
-        close
     } = props
-
-    const { mathFunc } = state
 
     const { setCurrentView } = useContext(ViewContext)
 
@@ -30,29 +23,12 @@ const FractionKeys = (props) => {
         })
     },[])
 
-    const input = (e) => {
-        e.preventDefault()
-        const { name, value } = e.target
-        const currentValueArr = value.split('')
-        const valString = currentValueArr.slice(1).join('')
-        const length = currentValueArr.length
-        
-        if (length === 0 || isNaN(valString)) {
-            execute(e,'noticeContent',errorMessage)
-            return
-            
-        } else {inputHandler(e)}
-    }
-
     return (
         <KeyBox>
 
-            <InputField
-                inputClass={'small'}
-                type='number'
-                onChange={(e) => input(e)}
-                value={mathFunc}
-                name="mathFunc"
+            <NumberPad
+                state={state}
+                setState={setState}
             />
 
             <Button 
