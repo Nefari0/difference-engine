2347800c15ea126a321d4f86db9dcfcee951a1e7
Nefari0 <chrisdemontigny@yahoo.com>
2023-08-24 21:42:01 -0600

    Aligned content to center and bottom of view port

--- diff-engine/src/Components/Graphs/Calculators/Converters/Fractions/frac.styles.jsx
+++ diff-engine/src/Components/Graphs/Calculators/Converters/Fractions/frac.styles.jsx
@@ -6,17 +6,20 @@ import { backgroundColors } from "../../../global.styles";
 const { paper,dark } = backgroundColors
 
 export const FracDisplay = styled.section`
-    position:absolute;
+    position:relative;
+    margin:auto;
     font-size:20px;
     bottom:40px;
-    width:80%;
-    height:40%;
-    // background-color:#fff;
+    width:100%;
+    height:100%;
     background-color:${({darkmode}) => darkmode ? dark:paper};
     color:${({darkmode}) => darkmode ? '#fff':'black'};
     border: solid 1px;
     border-radius:5px;
     z-index:1;
+    display:flex;
+    flex-direction:column;
+    justify-content:flex-end;
 
     p {
         font-size:40px;
