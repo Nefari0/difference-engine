2bdd4682e967ea76049077284359417a06a82441
Nefari0 <chrisdemontigny@yahoo.com>
2023-11-21 21:13:51 -0700

    Standard coefficients

---
+++ diff-engine/src/Components/Graphs/coefficients.js
@@ -0,0 +1,5 @@
+export const coeffs = {
+    c_1:299792458,  // Speed of light in meters per second
+    c_2:983571056, // Speed of light in feet per second
+    // c_2:186000, // Speed of light in miles per second
+}
